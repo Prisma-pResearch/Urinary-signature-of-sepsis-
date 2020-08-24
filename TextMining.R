5c9fe4c749121b0413f987f048c7a8688fed3b7f
sabyasachi0212 <36046666+sabyasachi0212@users.noreply.github.com>
2020-08-24 11:02:57 -0400

    Update TextMining.R

--- TextMining.R
+++ TextMining.R
@@ -10,7 +10,7 @@ textMining <- function(geneset){
       string<-paste(keyterms[i], string, sep=" ")#adding key terms to the front
       
       rSearch<-entrez_search(db="pubmed", term=string, retmax=90000) #entrez search
-      write.csv(rSearch$ids, paste("S:/PRISMAP Studies/PROJECTS/C2 Urinary Signature of Sepsis/3 Results/Transcriptome Analysis Console/WithoutUTI_onlyRMA/Results/Text Mining_selected probes_without Lasso_only Sepsis/", geneset[j], keyterms[i], ".csv", sep="")) #write the pubmed ids in a csv file
+      write.csv(rSearch$ids, paste("./3 Results/Transcriptome Analysis Console/WithoutUTI_onlyRMA/Results/Text Mining_selected probes_without Lasso_only Sepsis/", geneset[j], keyterms[i], ".csv", sep="")) #write the pubmed ids in a csv file
     }
   return(rSearch)
 }
