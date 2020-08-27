21cd9dd8555f260cc857ab5f76007d44b95491e5
sabyasachi0212 <36046666+sabyasachi0212@users.noreply.github.com>
2020-08-27 17:43:24 -0400

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -5,8 +5,10 @@
 
 
 def SplitX_y(data):
-    X = data.drop(['Outcome'], axis=1); y = data['Outcome']
-    return X, y
+    x = data.drop(['Outcome'], axis=1)
+    y = data['Outcome']
+    
+    return x, y
 
 
 # In[2]:
@@ -17,5 +19,6 @@ def Find_GeneDirection(gene_list, limma):
     for count, x in enumerate(gene_list):
         target = positive_list if(np.sign(limma['Fold Change'][x]) == 1) else negative_list
         target.append(x)
+    
     return positive_list, negative_list
 
