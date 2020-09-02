6646b8ec962548dd3c4d7508f9fffe8a39c8ce82
sabyasachi0212 <36046666+sabyasachi0212@users.noreply.github.com>
2020-09-02 18:54:14 -0400

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -5,6 +5,7 @@
 
 
 def SplitX_y(data):
+    """Separate X from y"""
     x = data.drop(['Outcome'], axis=1)
     y = data['Outcome']
     
@@ -15,6 +16,12 @@ def SplitX_y(data):
 
 
 def Find_GeneDirection(gene_list, limma):
+    """Find upregulated and downregulated genes
+    
+    Arguments:
+    gene_list -- Gene list of interest
+    limma -- LIMMA analysis on whole genome
+    """
     positive_list, negative_list = [], []
     for count, x in enumerate(gene_list):
         target = positive_list if(np.sign(limma['Fold Change'][x]) == 1) else negative_list
